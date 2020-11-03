545baf95dceef8d7448e8ad7c52b4568760ba714
Izzy <65364798+queenIsabella99@users.noreply.github.com>
2020-11-03 11:59:54 -0500

    nothing works pt2

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ $(document).ready(function () {
     if(localStorage.getItem("searches") != null){
         lastSearches = JSON.parse(localStorage.getItem("searches"));
 
+        console.log(lastSearches);
+        var historyDiv = $("#history");
+
         for (var i = 0; i < lastSearches; i++) {
-            var btn = $("<button").text(lastSearches[i]);
+            var btn = $("<button>").text(lastSearches[i]);
             historyDiv.append(btn);
         }
     }
@@ -18,10 +21,12 @@ $(document).ready(function () {
             lastSearches.push(cityText);
             localStorage.setItem("searches", JSON.stringify(lastSearches));
 
+            console.log(lastSearches);
+
             historyDiv.empty();
 
             for (var i = 0; i < lastSearches; i++) {
-                var btn = $("<button").text(lastSearches[i]);
+                var btn = $("<button>").text(lastSearches[i]);
                 historyDiv.append(btn);
             }
         }
