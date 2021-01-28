4b52bde5e6115a8f85b2d751e2e4226cd13e066a
Andrea <65364798+beandrea@users.noreply.github.com>
2021-01-28 17:13:50 -0500

    found correct routes

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     var apiKey = "91740392b403e980dc4396057af69b35";
 
-    $("search").on("click", function() {
-        var value = $("#city").val();
-        $("#city").val("");
+    $("#search-btn").on("click", function () {
+        var value = $("#search-val").val();
+        $("#search-val").val("");
         searchWeather(value);
     });
 
-    $(".history").on("click", "li", function() {
+    $(".history").on("click", "li", function () {
         searchWeather($(this).text());
     });
 
@@ -21,9 +21,11 @@ $(document).ready(function () {
             type: "GET",
             url: "https://api.openweathermap.org/data/2.5/forecast?q=" + value + "&appid=" + apiKey,
             dataType: "json",
-            success: function(data) {
-                if(history.indexOf(value) === -1){
-                    history.pushState(value);
+            success: function (data) {
+                console.log(data);
+
+                if (history.indexOf(value) === -1) {
+                    history.push(value);
                     localStorage.setItem("history", JSON.stringify(history));
 
                     makeRow(value);
@@ -33,10 +35,11 @@ $(document).ready(function () {
 
                 var title = $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ") ");
                 var card = $("<div>").addClass("card");
-                var wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + " MPH");
-                var humid = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity +"%");
+                var wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.list[0].wind.speed + " MPH");
+                var humid = $("<p>").addClass("card-text").text("Humidity: " + data.list[0].main.humidity + "%");
+                var temp = $("<p>").addClass("card-text").text("Temperature: " + data.main.temp + " °F");
                 var cardBody = $("<div>").addClass("card-body");
-                var img = $("<img>").attr("src", "https://openweather.org/img/w/" + data.weather[0].icon + ".png");
+                var img = $("<img>").attr("src", "https://openweather.org/img/w/" + data.list[0].weather[0].icon + ".png");
 
                 title.append(img);
                 cardBody.append(title, temp, humid, wind);
@@ -54,11 +57,11 @@ $(document).ready(function () {
             type: "GET",
             url: "http://api.openweathermap.org/data/2.5/forecast?q=" + value + "&appid=" + apiKey + "&units=imperial",
             dataType: "json",
-            success: function(data) {
+            success: function (data) {
                 $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\">");
 
-                for(let i = 0; i < data.list.length; i++){
-                    if(data.list[i].dt_text.indexOf("15:00:00") !== -1){
+                for (let i = 0; i < data.list.length; i++) {
+                    if (data.list[i].dt_text.indexOf("15:00:00") !== -1) {
                         var col = $("<div>").addClass("col-md-2");
                         var card = $("<div>").addClass("card bg-primary text-white");
                         var body = $("<div>").addClass("card-body p-2");
@@ -84,9 +87,9 @@ $(document).ready(function () {
                 var uv = $("<p>").text("UV Index: ");
                 var btn = $("<span>").addClass("btn btn-sm").text(data.value);
 
-                if(data.value < 3) {
+                if (data.value < 3) {
                     btn.addClass("btn-success");
-                } else if(data.value < 7) {
+                } else if (data.value < 7) {
                     btn.addClass("btn-warning");
                 } else {
                     btn.addClass("btn-danger");
@@ -99,11 +102,11 @@ $(document).ready(function () {
 
     var history = JSON.parse(localStorage.getItem("history")) || [];
 
-    if(history.length > 0) {
+    if (history.length > 0) {
         searchWeather(history[history.length - 1]);
     }
 
-    for(let i = 0; i < history.length; i++){
+    for (let i = 0; i < history.length; i++) {
         makeRow(history[i]);
     }
 });
